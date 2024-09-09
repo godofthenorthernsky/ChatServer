c54f3f680497a9ba309affbcda1b3a99a7b83307
doubley <18981253133@163.com>
2024-09-09 16:56:05 +0800

    提交项目

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/projects/CHAT/src/server/chatserver.cpp" "src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o.d"
+  "/root/projects/CHAT/src/server/chatservice.cpp" "src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o.d"
+  "/root/projects/CHAT/src/server/db/db.cpp" "src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o.d"
+  "/root/projects/CHAT/src/server/friendmodel.cpp" "src/server/CMakeFiles/ChatServer.dir/friendmodel.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/friendmodel.cpp.o.d"
+  "/root/projects/CHAT/src/server/groupmodel.cpp" "src/server/CMakeFiles/ChatServer.dir/groupmodel.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/groupmodel.cpp.o.d"
+  "/root/projects/CHAT/src/server/main.cpp" "src/server/CMakeFiles/ChatServer.dir/main.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/main.cpp.o.d"
+  "/root/projects/CHAT/src/server/offlinemessagemodel.cpp" "src/server/CMakeFiles/ChatServer.dir/offlinemessagemodel.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/offlinemessagemodel.cpp.o.d"
+  "/root/projects/CHAT/src/server/redis.cpp" "src/server/CMakeFiles/ChatServer.dir/redis.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/redis.cpp.o.d"
+  "/root/projects/CHAT/src/server/usermodel.cpp" "src/server/CMakeFiles/ChatServer.dir/usermodel.cpp.o" "gcc" "src/server/CMakeFiles/ChatServer.dir/usermodel.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
